eb4a3263bb336963f82e3e3ee7b0fdb75af42a8a
Luke Su <luke.su02@gmail.com>
2024-02-06 17:34:17 +1100

    Tools: Updated create env file tool

--- tools/createEnvFile.js
+++ tools/createEnvFile.js
@@ -1,22 +1,58 @@
-/** Import Javascript Modules */
+/** Create Env File 
+ *  
+ * @brief Lightweight utility, designed to generate the environment variables 
+ * of this project. 
+ * 
+ */
+
+/** Module Defintions */
 const fs = require('fs');
+const reader = require('readline-sync');
 
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+/** Private Types */
+const ENV_FILE_PATH = ".env";
 
-var discordTokenEnvArg = '';
-const FILE_SYSTEM_PATH = ".env";
+/** Environemnt Arugment Schema */
+let environmentArgs = {
+  "discordAccessToken" : {
+    "alias" : "DISCORD_TOKEN",
+    "value" : null
+  },
+};
 
-readline.question(`Enter Discord Token : `, name => {
-    discordTokenEnvArg = "DISCORD_TOKEN=" + "\"" + name + "\"";
-    fs.writeFile(FILE_SYSTEM_PATH, discordTokenEnvArg, (err) => {
+/** Main Function */
+function main()
+{
+    /** Request Environment Variables*/
+    environmentArgs.discordAccessToken.value = reader.question('Discord Access Token : ', { hideEchoBack: true });
+
+    /** Generate file from requested Variables */
+    createEnvironmentFile(environmentArgs,ENV_FILE_PATH);
+}
+
+/** Methods */
+createEnvironmentFile = (environmentArguments, path) => {
+    // Overwrite File
+    fs.writeFile(path,"", (err) => {
         if (err) throw err;
         else
         {
-           console.log(".env file cread - Access token Updated");
+           console.log("Created Env file");
         }
-     })
-    readline.close();
-});
+    });
+
+    for (const [ _ , value] of Object.entries(environmentArguments)) 
+    {
+        fs.appendFile(path, `${value.alias}=\"${value.value}\"`, (err) => {
+            if (err) throw err;
+            else
+            {
+                console.log(`${value.alias} added to Environment Variables.`)
+            }
+        });
+        //console.log(`${value.alias}: ${value.value}`);
+    }
+}
+
+/** Function Calls */
+main();
